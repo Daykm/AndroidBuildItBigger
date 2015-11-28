121d9f2df2468260cd63dc5ad20ce72b012f7b59
Daykm <daykmsc@gmail.com>
2015-11-28 11:32:57 -0500

    Add module

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.Jokes;
+import com.example.daykm.jokesactivity.JokesActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -48,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, JokesActivity.class);
+                intent.putExtra(JokesActivity.KEY, s);
+                startActivity(intent);
             }
         };
 
---
+++ jokesactivity/src/main/java/com/example/daykm/jokesactivity/JokesActivity.java
@@ -0,0 +1,17 @@
+package com.example.daykm.jokesactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokesActivity extends AppCompatActivity {
+
+    public static final String KEY = "INTENT_KEY";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+
+        ((TextView) findViewById(R.id.joke)).setText(getIntent().getExtras().getString(KEY));
+    }
+}
